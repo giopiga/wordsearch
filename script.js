3fe226607a524ae82fcac259db7fd13304f32d91
Michele Bucelli <michele.bucelli@polimi.it>
2023-10-15 14:38:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,181 @@
+// Grid content. ///////////////////////////////////////////////////////////////
+
+const grid_width = 16;
+const grid_height = 15;
+
+const grid =
+    'MMILANOBSILENZ&GAABIHSUILLENIRAMRETTOPYRRAHTENAFIVNIPOTIIEADETRRONBNTLNI' +
+    'DMONEEEBKHAISISSAOIMZLUAACNTBTEGGDALOIMBRAGMAEUBUTASOGEYTEANOCYVIVOENPNS' +
+    'IBAMENICARESSETIHATTNIABARBEROATCVAHICQVUBOUQUETOOOATOLSIRVAIHCEIJAIDATA' +
+    'SCIENCERGSFPTAPPAEKLIRRO';
+
+const words_1 = [
+  'AIDA',        'ALVAROSOLER', 'APPA',      'ASSISI',      'BABYSITTER',
+  'BAMBINI',     'BANG',        'BARBERO',   'BOUQUET',     'CATAN',
+  'CHIAVRIS',    'DATASCIENCE', 'GIOCHI',    'HARRYPOTTER', 'JOHNNYBGOODE',
+  'JOVABEACH',   'LISBONA',     'MARINELLI', 'MARIOKART',   'MATEMATICA',
+  'MATIZ',       'MENTA',       'MILANO',    'NIPOTI',      'POLIMIRUN',
+  'POLITECNICO', 'RILKE',       'SFP',       'SHIBA',       'TESSERACINEMA',
+  'UDINE',       'VALZER'
+];
+
+// State. //////////////////////////////////////////////////////////////////////
+
+// Toggle "dragging" mode. Dragging mode is initiated by clicking (tapping?) on
+// a letter and is terminated by releasing the click (on that or some other
+// letter).
+let dragging = false;
+
+let dragging_start = [-1, -1];
+let dragging_end = [-1, -1];
+let dragging_direction = [0, 0];
+let dragging_length = 0;
+
+let found_words = Array(words_1.length);
+
+// Initialization. /////////////////////////////////////////////////////////////
+
+let setup = function() {
+  found_words.fill(false);
+
+  // Initialize the UI.
+  let grid_wrapper = document.getElementById('grid-wrapper');
+  let word_list_1 = document.getElementById('word-list-1');
+
+  let template_letter = document.getElementById('template-letter');
+  let template_word = document.getElementById('template-word');
+
+  // Generate the grid of letters.
+  template_letter.remove();
+  for (let i = 0; i < grid_height; ++i) {
+    for (let j = 0; j < grid_width; ++j) {
+      const idx = j + grid_width * i;
+
+      let letter_span = template_letter.cloneNode();
+
+      letter_span.id = 'letter-' + i + '-' + j;
+      letter_span.textContent = grid.charAt(idx);
+
+      letter_span.onmousedown = function() {
+        dragging = true;
+        dragging_start = [i, j];
+      };
+
+      letter_span.onmouseenter = function() {
+        if (dragging) {
+          dragging_end = [i, j];
+
+          const diff = [
+            dragging_end[0] - dragging_start[0],
+            dragging_end[1] - dragging_start[1]
+          ];
+
+          const theta = Math.atan2(diff[1], diff[0]);
+          const pi = Math.PI;
+
+          if (-pi / 8 < theta && theta <= pi / 8) {
+            dragging_direction = [1, 0];
+            dragging_length = dragging_end[0] - dragging_start[0];
+          } else if (pi / 8 < theta && theta <= 3 * pi / 8) {
+            dragging_direction = [1, 1];
+            dragging_length = dragging_end[0] - dragging_start[0];
+          } else if (3 * pi / 8 < theta && theta <= 5 * pi / 8) {
+            dragging_direction = [0, 1];
+            dragging_length = dragging_end[1] - dragging_start[1];
+          } else if (5 * pi / 8 < theta && theta <= 7 * pi / 8) {
+            dragging_direction = [-1, 1];
+            dragging_length = dragging_end[1] - dragging_start[1];
+          } else if (7 * pi / 8 < theta || theta < -7 * pi / 8) {
+            dragging_direction = [-1, 0];
+            dragging_length = dragging_start[0] - dragging_end[0];
+          } else if (-7 * pi / 8 <= theta && theta < -5 * pi / 8) {
+            dragging_direction = [-1, -1];
+            dragging_length = dragging_start[0] - dragging_end[0];
+          } else if (-5 * pi / 8 <= theta && theta < -3 * pi / 8) {
+            dragging_direction = [0, -1];
+            dragging_length = dragging_start[1] - dragging_end[1];
+          } else if (-3 * pi / 8 <= theta && theta < -1 * pi / 8) {
+            dragging_direction = [1, -1];
+            dragging_length = dragging_start[1] - dragging_end[1];
+          }
+
+          dragging_length += 1;
+
+          $('.dragged').removeClass('dragged');
+
+          let cur = dragging_start.slice(0);
+          for (let i = 0; i < dragging_length; ++i) {
+            $('#letter-' + cur[0] + '-' + cur[1]).addClass('dragged');
+
+            cur[0] += dragging_direction[0];
+            cur[1] += dragging_direction[1];
+          }
+        }
+      };
+
+      letter_span.onmouseup = function() {
+        if (!dragging) return;
+
+        dragging = false;
+
+        // Retrieve the dragged word.
+        let word = '';
+        {
+          let cur = dragging_start.slice(0);
+          for (let i = 0; i < dragging_length; ++i) {
+            const idx = cur[1] + grid_width * cur[0];
+            word += grid[idx];
+
+            cur[0] += dragging_direction[0];
+            cur[1] += dragging_direction[1];
+          }
+        }
+
+        // Check if it belongs to the list of words.
+        let found_word_idx = undefined;
+
+        for (let i = 0; i < words_1.length; ++i) {
+          if (word == words_1[i]) {
+            found_words[i] = true;
+            found_word_idx = i;
+            break;
+          }
+        }
+
+        $('.dragged').removeClass('dragged');
+
+        // If the word is correct, we update the corresponding word list element
+        // as well as the letters in the grid.
+        if (found_word_idx != undefined) {
+          $('#word-' + found_word_idx).addClass('found');
+
+          let cur = dragging_start.slice(0);
+          for (let i = 0; i < dragging_length; ++i) {
+            $('#letter-' + cur[0] + '-' + cur[1]).addClass('found');
+
+            cur[0] += dragging_direction[0];
+            cur[1] += dragging_direction[1];
+          }
+        }
+      };
+
+      grid_wrapper.appendChild(letter_span);
+    }
+
+    const br = document.createElement('br');
+    grid_wrapper.appendChild(br);
+  }
+
+  // Generate the list of words.
+  template_word.remove();
+  for (let i = 0; i < words_1.length; ++i) {
+    const word = words_1[i];
+
+    let word_span = template_word.cloneNode();
+
+    word_span.id = 'word-' + i;
+    word_span.textContent = word;
+
+    word_list_1.appendChild(word_span);
+  }
+};
